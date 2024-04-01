fb27e8523202dd9827c145793a30bc324d6e5aa6
CatLazy <115200650+RWzbrtcat@users.noreply.github.com>
2024-04-01 11:42:50 +0800

    Add files via upload

---
+++ Java/Sorted List/treemap.java
@@ -0,0 +1,24 @@
+import java.util.TreeMap;
+
+public class treemap {
+    public int minimumDistance(int[][] points) {
+        TreeMap<Integer, Integer> xs = new TreeMap<>();
+        TreeMap<Integer, Integer> ys = new TreeMap<>();
+        for(int[] p: points){
+            xs.merge(p[0] + p[1], 1, Integer::sum);
+            ys.merge(p[1] - p[0], 1, Integer::sum);
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int[] p: points){
+            int x = p[0] + p[1], y = p[1] - p[0];
+            if(xs.get(x) == 1) xs.remove(x);
+            else xs.merge(x, -1, Integer::sum);
+            if(ys.get(y) == 1) ys.remove(y);
+            else ys.merge(y, -1, Integer::sum);
+            ans = Math.min(ans, Math.max(xs.lastKey() - xs.firstKey(), ys.lastKey() - ys.firstKey()));
+            xs.merge(x, 1, Integer::sum);
+            ys.merge(y, 1, Integer::sum);
+        }
+        return ans;
+    }
+}
